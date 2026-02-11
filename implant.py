ffab8970f75f7dbe335d031f56dafca7dfea3644
Ivan Mladenov <imladenov@icloud.com>
2026-02-10 22:46:56 -0500

    Push to test on VM.

--- implant.py
+++ implant.py
@@ -6,37 +6,58 @@ import netaddr
 import socket
 import struct
 
-ifaces: list[str] = netifaces.interfaces()
-subnets: list[netaddr.IPNetwork] = []
-host_mac: bytes = bytes.fromhex(
-    netifaces.ifaddresses('eth0')[netifaces.AF_LINK][0]['addr'].replace(':', '')
-)
-host_ip: bytes = socket.inet_aton(netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr'])
+# Grab IPv4 ranges for each subnet for all (non-local) interfaces
+def get_subnets() -> list[netaddr.IPNetwork]:
+    ifaces = netifaces.interfaces()
+    subnets = []
 
-# Grab IPv4 ranges for each subnet for all (non-local) interfaces 
-for iface in ifaces:
-    if iface == 'lo':
-        continue
-    
-    addr = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]
-    subnet = netaddr.IPNetwork(addr['addr'])
-    subnet.netmask = (addr['netmask'])
-    subnets.append(subnet)
+    for iface in ifaces:
+        if iface == 'lo':
+            continue
+        
+        addr = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]
+        subnet = netaddr.IPNetwork(addr['addr'])
+        subnet.netmask = (addr['netmask'])
+        subnets.append(subnet[:-1]) # strip broadcast IP
 
+    return subnets
 
 # ARP frame without target IP (fill in as needed)
-ethernet_header  = b'\xFF\xFF\xFF\xFF\xFF\xFF'  # Broadcast MAC
-ethernet_header += host_mac 
-ethernet_header += b'\x08\x06'                  # Ethernet type
-arp_packet  = b'\x00\x01'                       # Ethernet type
-arp_packet += b'\x08\x00'                       # IP protocol
-arp_packet += b'\x06'                           # MAC size
-arp_packet += b'\x04'                           # IP size
-arp_packet += host_mac
-arp_packet += host_ip
-arp_packet += b'\x00\x00\x00\x00\x00\x00'       # Target MAC
-frame = ethernet_header + arp_packet
+def build_frame() -> bytes:
+    host_mac: bytes = bytes.fromhex(
+        netifaces.ifaddresses('eth0')[netifaces.AF_LINK][0]['addr'].replace(':', '')
+    )
+    host_ip: bytes = socket.inet_aton(netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr'])
 
-for subnet in subnets:
-    for ip in subnet:
-        print(socket.inet_aton(str(ip)))
+    ethernet_header  = b'\xFF\xFF\xFF\xFF\xFF\xFF'  # Broadcast MAC
+    ethernet_header += host_mac 
+    ethernet_header += b'\x08\x06'                  # ARP type
+    arp_packet  = b'\x00\x01'                       # Ethernet type
+    arp_packet += b'\x08\x00'                       # IP protocol
+    arp_packet += b'\x06'                           # MAC size
+    arp_packet += b'\x04'                           # IP size
+    arp_packet += b'\x00\x01'                       # Operation type
+    arp_packet += host_mac
+    arp_packet += host_ip
+    arp_packet += b'\x00\x00\x00\x00\x00\x00'       # Target MAC
+    frame = ethernet_header + arp_packet
+    return frame
+
+# TODO: Read response, parallelize 
+def run_scan() -> None:
+    subnets = get_subnets()
+    frame = build_frame()
+
+    sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW)
+    sock.bind(('eth0', 0))
+
+    for subnet in subnets:
+        for ip in subnet:
+            target_ip = socket.inet_aton(str(ip))
+            new_frame = frame + target_ip
+            sock.send(new_frame)
+
+    sock.close()
+
+if __name__ == "__main__":
+    run_scan()
